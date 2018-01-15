1947081f1671fd32b83295c9ff8580c9869f67e6
dcamriff <driffelatl@gmail.com>
2018-01-14 20:19:10 -0500

    working on navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,80 @@
-import React, { Component } from 'react';
-import { FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch } from 'react-icons/lib/fa';
+import React, {Component} from 'react';
+import {FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch} from 'react-icons/lib/fa';
 import styled from 'styled-components';
 
-class Navbar extends Component{
-  render(){
+const Nav = styled.div `
+background-color: white;
+display: flex;
+justify-content: space-between;
+webkit-box-align: center;
+align-items: center;
+border: 0 solid #000;
+padding: 26px 5vw;
+width: 90vw;
+height: 25px;
+border-bottom: 1px solid rgba(0,0,0,.0975);
+font-family: "Open Sans";
+
+p{
+  font-size: 1.75rem;
+  font-family: "Oleo Script", cursive;
+  margin: 0px;
+  display: block; {
+    -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em:
+    -webkit-margin-start: 0px;
+    -webit-margin-end: 0px;
+  }
+  svg{
+    margin-bottom: 5px;
+  }
+}
+`
+const SearchContainer = styled.div`
+background: #fafafa;
+border: 1px solid #dbdbdb;
+width: 25%;
+padding: 7px;
+border-radius: 3px;
+color: #999;
+svg{
+    margin-bottom: 5px;
+  }
+
+input{
+  border: none;
+  font-weight: 300;
+  background: transparent;
+  text-align: center;
+}
+`
+const IconsContainer = styled.div`
+width: 25%;
+display: flex;
+justify-content: space-around;
+`
+
+/* const FaCompass = styled.div`
+
+` */
+
+
+class Navbar extends Component {
+  render() {
     return (
-      <div>
-        <h3><FaInstagram /> | Instaclone</h3>
-        <FaSearch/><input type="text"/>
-        <div>
-          <FaCompass />
-          <FaHeartO />
-          <FaUser />
-        </div>
-      </div>
-    );
+      <Nav>
+        <p><FaInstagram/>
+          | Instaclone</p>
+        <SearchContainer>
+          <FaSearch/><input type="text" placeholder="Search"/>
+        </SearchContainer>
+        <IconsContainer>
+          <FaCompass/>
+          <FaHeartO/>
+          <FaUser/>
+        </IconsContainer>
+      </Nav>
+    )
   }
 }
 
